c45e7f8fd1253ddd04b9d532bc4847e507bf728e
Zayneb Messaoudi <146714154+zaineb291@users.noreply.github.com>
2023-12-12 17:46:30 +0100

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_TuniCamp_work', label: 'How TuniCamp Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Real maps can be offline',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+  {
+    title: 'Set an adventure schedule',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "Schedule an adventure with friends. On holidays, there are many interesting offers from TuniCamp. That way, there's no more discussion",
+  },
+  {
+    title: 'Technology using augment reality',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
+  },
+  {
+    title: 'Many new locations every month',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Lots of new locations every month, because we have a big local community of campers who share their best experiences with camping',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About TuniCamp',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing xixixi', 'Hiking TuniCamp', 'TuniCamp kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '+216 98 765 432' },
+    { label: 'Email Officer', value: 'TuniCamp@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
